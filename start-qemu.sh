ba4da3880ae994d34b08f116b2fe352b2e27fda2
Aaron Ang <aaron.angyd@gmail.com>
2025-03-04 21:59:25 -0800

    create benchmark script

--- start-qemu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# QEMU command to start the virtual machine
-qemu-system-x86_64 \
-  -kernel bzImage \
-  -initrd initrd.img-6.8.0-1021-aws \
-  -m 1G \
-  -append "console=ttyS0" \
-  -nographic
