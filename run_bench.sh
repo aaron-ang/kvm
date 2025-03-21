05923f1d93bf38d9990a6ce867b64eb547d018b8
Aaron Ang <aaron.angyd@gmail.com>
2025-03-21 13:20:43 -0700

    dont turn off EPT

--- run_bench.sh
+++ run_bench.sh
@@ -131,7 +131,7 @@ run_vm() {
 cd /mnt
 
 echo 0 | sudo tee /sys/module/kvm/parameters/tdp_mmu
-echo 0 | sudo tee /sys/module/kvm_intel/parameters/ept
+# echo 0 | sudo tee /sys/module/kvm_intel/parameters/ept
 echo 1 | sudo tee /sys/module/kvm/parameters/lru_mmu
 echo 32 | sudo tee /sys/module/kvm/parameters/min_alloc_pages
 
